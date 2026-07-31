a164381161b5be40fe6c589ac1277142d7e903d9
JIEUN LEE <lje030522@gmail.com>
2026-07-31 15:52:36 +0900

    chore(backend): 로컬 개발 환경 CORS 설정 추가 (#24) (#26)

--- backend/src/main/java/me/nawa/config/ServletConfig.java
+++ backend/src/main/java/me/nawa/config/ServletConfig.java
@@ -31,4 +31,13 @@ public class ServletConfig implements WebMvcConfigurer {
 //                .addResourceHandler("/resources/**")     // url이 /resources/로 시작하는 모든 경로
 //                .addResourceLocations("/resources/");    // webapp/resources/경로로 매핑
     }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:5173")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedHeaders("Authorization", "Content-Type")
+            .allowCredentials(true);
+    }
 }
