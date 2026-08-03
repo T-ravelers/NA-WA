0f190c914325b967d262011980049083f640d2f6
Mingyu Joung <samu9nai@gmail.com>
2026-08-03 15:14:56 +0900

    test(auth): Swagger 테스트 인증 의존성 보완 (#23)

--- backend/src/test/java/me/nawa/config/SwaggerConfigTest.java
+++ backend/src/test/java/me/nawa/config/SwaggerConfigTest.java
@@ -6,6 +6,9 @@ import me.nawa.auth.cookie.AuthCookieManager;
 import me.nawa.auth.oauth.authorization.OAuthAuthorizationService;
 import me.nawa.auth.oauth.callback.OAuthCallbackResult;
 import me.nawa.auth.oauth.callback.OAuthCallbackService;
+import me.nawa.auth.profile.AuthMeResponse;
+import me.nawa.auth.profile.AuthMeService;
+import me.nawa.auth.profile.AuthMemberProfile;
 import me.nawa.auth.token.AuthTokenService;
 import me.nawa.auth.token.AuthTokens;
 import me.nawa.common.exception.ErrorCode;
@@ -156,6 +159,13 @@ class SwaggerConfigTest {
                     );
                 }
             };
+        }
+
+        @Bean
+        AuthMeService authMeService() {
+            return memberId -> new AuthMeResponse(
+                    AuthMemberProfile.active(memberId, false)
+            );
         }
     }
 
