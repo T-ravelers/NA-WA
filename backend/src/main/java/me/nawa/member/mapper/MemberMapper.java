da2bbd3f8a2efdf1abf693a11155933998bb2244
Mingyu Joung <samu9nai@gmail.com>
2026-08-05 21:56:22 +0900

    feat(member): 회원 프로필 조회 매퍼 및 도메인 추가 (#57)

--- backend/src/main/java/me/nawa/member/mapper/MemberMapper.java
+++ backend/src/main/java/me/nawa/member/mapper/MemberMapper.java
@@ -1,7 +1,13 @@
 package me.nawa.member.mapper;
 
+import me.nawa.member.domain.MemberProfile;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface MemberMapper {
+
+    MemberProfile findProfile(@Param("memberId") long memberId);
+
+    boolean existsActiveCurrency(@Param("currencyCode") String currencyCode);
 }
