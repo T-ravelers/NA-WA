d398522f884b44145c31d0e12b6723224e18fa53
TIRIOM <coolryan1004@gmail.com>
2026-08-13 16:44:55 +0900

    [Feat] Journey 타임라인 응답에 appointmentId 노출 (#189)
    appointment 객체에 확정된 약속 ID를 추가해 View companions CTA가 이동할
    대상을 알 수 있게 한다. 도메인과 SQL은 이미 값을 갖고 있어 DTO와 조립
    Service만 수정한다.

--- backend/src/main/java/me/nawa/journey/dto/response/JourneyTimelineAppointmentResponse.java
+++ backend/src/main/java/me/nawa/journey/dto/response/JourneyTimelineAppointmentResponse.java
@@ -1,6 +1,7 @@
 package me.nawa.journey.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.annotations.ApiModelProperty;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -12,6 +13,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class JourneyTimelineAppointmentResponse {
+
+    @ApiModelProperty(value = "확정된 약속 ID", example = "900")
+    private Long appointmentId;
 
     @JsonFormat(
         shape = JsonFormat.Shape.STRING,
